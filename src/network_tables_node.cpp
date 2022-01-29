9ccded422faed3537a9403a08012fe0c6fa17918
Robert Hilton <robert.a.hilton.jr@gmail.com>
2022-01-29 08:38:16 +0000

    Add services for nt

--- src/network_tables_node.cpp
+++ src/network_tables_node.cpp
@@ -3,13 +3,135 @@
 #include "networktables/NetworkTable.h"
 #include "networktables/NetworkTableInstance.h"
 
+#include "network_tables_node/NTGetBool.h"
+#include "network_tables_node/NTGetBoolArray.h"
+#include "network_tables_node/NTGetDouble.h"
+#include "network_tables_node/NTGetDoubleArray.h"
+#include "network_tables_node/NTGetString.h"
+#include "network_tables_node/NTGetStringArray.h"
+#include "network_tables_node/NTGetRaw.h"
+
+#include "network_tables_node/NTSetBool.h"
+#include "network_tables_node/NTSetBoolArray.h"
+#include "network_tables_node/NTSetDouble.h"
+#include "network_tables_node/NTSetDoubleArray.h"
+#include "network_tables_node/NTSetString.h"
+#include "network_tables_node/NTSetStringArray.h"
+#include "network_tables_node/NTSetRaw.h"
+
 #include <thread>
 #include <string>
 #include <mutex>
+#include <vector>
 
 ros::NodeHandle* node;
 
 nt::NetworkTableInstance networkTableInst;
+
+bool nt_getbool(network_tables_node::NTGetBool::Request &request, network_tables_node::NTGetBool::Response &response)
+{
+	response.output = networkTableInst.GetTable(request.table_name)->GetEntry(request.entry_name).GetBoolean(request.default_value);
+	return true;
+}
+
+bool nt_getboolarray(network_tables_node::NTGetBoolArray::Request &request, network_tables_node::NTGetBoolArray::Response &response)
+{
+	std::vector<int> defaultBoolVals;
+	for (size_t i = 0; i < request.default_value.size(); i++)
+	{
+		defaultBoolVals.push_back(request.default_value[i]);
+	}
+	std::vector<int> resp(networkTableInst.GetTable(request.table_name)->GetEntry(request.entry_name).GetBooleanArray(defaultBoolVals));
+	for (size_t i = 0; i < resp.size(); i++)
+	{
+		response.output.push_back(resp[i]);
+	}
+	return true;
+}
+
+bool nt_getdouble(network_tables_node::NTGetDouble::Request &request, network_tables_node::NTGetDouble::Response &response)
+{
+	response.output = networkTableInst.GetTable(request.table_name)->GetEntry(request.entry_name).GetDouble(request.default_value);
+	return true;
+}
+
+bool nt_getdoublearray(network_tables_node::NTGetDoubleArray::Request &request, network_tables_node::NTGetDoubleArray::Response &response)
+{
+	response.output = networkTableInst.GetTable(request.table_name)->GetEntry(request.entry_name).GetDoubleArray(request.default_value);
+	return true;
+}
+
+bool nt_getstring(network_tables_node::NTGetString::Request &request, network_tables_node::NTGetString::Response &response)
+{
+	response.output = networkTableInst.GetTable(request.table_name)->GetEntry(request.entry_name).GetString(request.default_value);
+	return true;
+}
+
+bool nt_getstringarray(network_tables_node::NTGetStringArray::Request &request, network_tables_node::NTGetStringArray::Response &response)
+{
+	response.output = networkTableInst.GetTable(request.table_name)->GetEntry(request.entry_name).GetStringArray(request.default_value);
+	return true;
+}
+
+bool nt_getraw(network_tables_node::NTGetRaw::Request &request, network_tables_node::NTGetRaw::Response &response)
+{
+	response.output = networkTableInst.GetTable(request.table_name)->GetEntry(request.entry_name).GetRaw(request.default_value);
+	return true;
+}
+
+
+
+
+bool nt_setbool(network_tables_node::NTSetBool::Request &request, network_tables_node::NTSetBool::Response &response)
+{
+	response.success = networkTableInst.GetTable(request.table_name)->GetEntry(request.entry_name).SetBoolean(request.value);
+	return response.success;
+}
+
+bool nt_setboolarray(network_tables_node::NTSetBoolArray::Request &request, network_tables_node::NTSetBoolArray::Response &response)
+{
+	std::vector<int> boolVals;
+	for (size_t i = 0; i < request.value.size(); i++)
+	{
+		boolVals.push_back(request.value[i]);
+	}
+	response.success = networkTableInst.GetTable(request.table_name)->GetEntry(request.entry_name).SetBooleanArray(boolVals);
+	return response.success;
+}
+
+bool nt_setdouble(network_tables_node::NTSetDouble::Request &request, network_tables_node::NTSetDouble::Response &response)
+{
+	response.success = networkTableInst.GetTable(request.table_name)->GetEntry(request.entry_name).SetDouble(request.value);
+	return response.success;
+}
+
+bool nt_setdoublearray(network_tables_node::NTSetDoubleArray::Request &request, network_tables_node::NTSetDoubleArray::Response &response)
+{
+	response.success = networkTableInst.GetTable(request.table_name)->GetEntry(request.entry_name).SetDoubleArray(request.value);
+	return response.success;
+}
+
+bool nt_setstring(network_tables_node::NTSetString::Request &request, network_tables_node::NTSetString::Response &response)
+{
+	response.success = networkTableInst.GetTable(request.table_name)->GetEntry(request.entry_name).SetString(request.value);
+	return response.success;
+}
+
+bool nt_setstringarray(network_tables_node::NTSetStringArray::Request &request, network_tables_node::NTSetStringArray::Response &response)
+{
+	response.success = networkTableInst.GetTable(request.table_name)->GetEntry(request.entry_name).SetStringArray(request.value);
+	return response.success;
+}
+
+bool nt_setraw(network_tables_node::NTSetRaw::Request &request, network_tables_node::NTSetRaw::Response &response)
+{
+	response.success = networkTableInst.GetTable(request.table_name)->GetEntry(request.entry_name).SetRaw(request.value);
+	return response.success;
+}
+
+
+
+
 
 int main(int argc, char **argv)
 {
@@ -28,6 +150,22 @@ int main(int argc, char **argv)
 	ros::NodeHandle n;
 
 	node = &n;
+
+	ros::ServiceServer service_getbool = node->advertiseService("nt_getbool", nt_getbool);
+	ros::ServiceServer service_getboolarray = node->advertiseService("nt_getboolarray", nt_getboolarray);
+	ros::ServiceServer service_getdouble = node->advertiseService("nt_getdouble", nt_getdouble);
+	ros::ServiceServer service_getdoublearray = node->advertiseService("nt_getdoublearray", nt_getdoublearray);
+	ros::ServiceServer service_getstring = node->advertiseService("nt_getstring", nt_getstring);
+	ros::ServiceServer service_getstringarray = node->advertiseService("nt_getstringarray", nt_getstringarray);
+	ros::ServiceServer service_getraw = node->advertiseService("nt_getraw", nt_getraw);
+
+	ros::ServiceServer service_setbool = node->advertiseService("nt_setbool", nt_setbool);
+	ros::ServiceServer service_setboolarray = node->advertiseService("nt_setboolarray", nt_setboolarray);
+	ros::ServiceServer service_setdouble = node->advertiseService("nt_setdouble", nt_setdouble);
+	ros::ServiceServer service_setdoublearray = node->advertiseService("nt_setdoublearray", nt_setdoublearray);
+	ros::ServiceServer service_setstring = node->advertiseService("nt_setstring", nt_setstring);
+	ros::ServiceServer service_setstringarray = node->advertiseService("nt_setstringarray", nt_setstringarray);
+	ros::ServiceServer service_setraw = node->advertiseService("nt_setraw", nt_setraw);
 
 	networkTableInst = nt::NetworkTableInstance::GetDefault();
 	ros::spin();
--- test/src/test_network_tables_node.cpp
+++ test/src/test_network_tables_node.cpp
@@ -1,8 +1,11 @@
 #include "test_network_tables_node.hpp"
 #include "network_tables_node.hpp"
+#include "network_tables_node/NTGetDouble.h"
 #include "ros/ros.h"
 
 #include <gtest/gtest.h>
+
+ros::NodeHandle* node;
 
 TEST(SampleTest, Test_Test)
 {
@@ -10,10 +13,41 @@ TEST(SampleTest, Test_Test)
     ASSERT_FALSE(0);
 }
 
+TEST(TestNTService, TestNTGetDouble)
+{
+    
+
+    //Usage example:
+    // ros::ServiceClient client = node->serviceClient<network_tables_node::NTGetDouble>("nt_getdouble", true);
+    // if (client)
+    // {
+    //     network_tables_node::NTGetDouble tx;
+    //     tx.request.table_name = "limelight";
+    //     tx.request.entry_name = "tx";
+    //     tx.request.default_value = 0.1;
+    //     if (client.call(tx))
+    //     {
+    //         ASSERT_EQ(tx.response.output, 0.1);
+    //     }
+    //     else
+    //     {
+    //         ROS_ERROR("Failed to run NT service");
+    //         ASSERT_TRUE(0); //Fail test
+    //     }
+    // }
+    // else
+    // {
+    //     //client = node->serviceClient<network_tables_node::NTGetDouble>("nt_getdouble", true);
+    //     ROS_ERROR("Failed to connect to NT service");
+    //     ASSERT_TRUE(0); //Fail test
+    // }
+}
+
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
     ros::init(argc, argv, "test_network_tables_node");
-    ros::NodeHandle nh;
+	ros::NodeHandle n;
+	node = &n;
     return RUN_ALL_TESTS();
 }
